93d9e5a3697d24dd88bad8eb6887de20cccb8b0c
jaumpedro214 <44421368+jaumpedro214@users.noreply.github.com>
2019-10-25 23:06:47 -0300

    Add vector function

--- Source/ode_solver_main.f90
+++ Source/ode_solver_main.f90
@@ -3,6 +3,34 @@ program ode_solver
     implicit none
     
     integer system_size
+    integer nodes_number
+    integer delta_x
+    integer intr_x
+
+    real(8), dimension(4) :: k
+    real(8), dimension(:), allocatable ::  old_sol, new_sol
     
+    system_size = 3
+    allocate( old_sol(system_size) )
+    allocate( new_sol(system_size) )
+
+    intr_x = 1.000
+    nodes_number = 10
+    delta_x = intr_x/(nodes_number-1)
+    
+
+
+    deallocate(old_sol)
+    deallocate(new_sol)
+
+    contains
+
+    function f(x, u) result( f_res )
+        implicit none
+        real(8), intent(in)  :: x
+        real(8), intent(in), dimension(:) :: u
+        real(8), dimension( size(u) ) :: f_res
+
+    end function f
 
 end program ode_solver
